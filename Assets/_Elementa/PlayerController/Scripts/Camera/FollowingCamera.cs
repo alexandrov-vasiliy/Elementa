0dc172bb1ba5b592e3d0aa68d5138e212cb3b83d
alexandrov-vasiliy <zowel.vasili@yandex.ru>
2024-12-06 16:16:15 +0300

    added player controller

---
+++ Assets/_Elementa/PlayerController/Scripts/Camera/FollowingCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [Header("Object for following")]
+    [SerializeField] private GameObject _mainCharacter;
+
+    [Header("Camera propertys")]
+    [SerializeField] private float _returnSpeed;                     
+    [SerializeField] private float _height;                     
+    [SerializeField] private float _rearDistance;                
+
+    private Vector3 currentVector;                 
+
+    void Start()
+    {                                    
+        transform.position = new Vector3(_mainCharacter.transform.position.x, _mainCharacter.transform.position.y + _height, _mainCharacter.transform.position.z - _rearDistance);
+        transform.rotation = Quaternion.LookRotation(_mainCharacter.transform.position - transform.position);
+    }
+
+    void Update()
+    {
+        CameraMove();
+    }
+
+    void CameraMove()
+    {
+        currentVector = new Vector3(_mainCharacter.transform.position.x, _mainCharacter.transform.position.y + _height, _mainCharacter.transform.position.z - _rearDistance);
+        transform.position = Vector3.Lerp(transform.position, currentVector, _returnSpeed * Time.deltaTime);
+    }
+}
